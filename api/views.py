1549313d0b498ad7f6648912ab11bc57d336a677
guatamsood15 <gauty22@gmail.com>
2022-09-25 22:19:33 +0530

    Adding CRUD views and Urls in REST Api

--- api/views.py
+++ api/views.py
@@ -4,6 +4,9 @@
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import NoteSerializer
+
+from .models import Note
 
 
 @api_view(['GET', 'POST', ])
@@ -41,3 +44,50 @@ def getRoutes(request):
         },
     ]
     return Response(routes)
+
+
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+
+    note = Note.objects.create(
+        body=data['body']
+    )
+
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteNote(request):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response('Note was deleted!')
